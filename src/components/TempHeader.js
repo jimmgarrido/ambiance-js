486686fd6472f762ecc603a684c9cca063a6bc0a
Jimmy Garrido <jimmygarrido@outlook.com>
2023-04-05 08:45:40 -0700

    Don't show forecast for now

--- src/components/TempHeader.js
+++ src/components/TempHeader.js
@@ -11,8 +11,8 @@ class TempHeader extends React.Component {
           <div className="col-sm-12">
             <div className="outdoor-temp-grid outdoor-temp">
               <h1>{this.props.data.tempf}°</h1>   
-              <h2 id="forecast-high">↑ {parseInt(this.props.forecast.daily.data[0].temperatureHigh)}°</h2>
-              <h2 id="forecast-low">↓ {parseInt(this.props.forecast.daily.data[0].temperatureLow)}°</h2>
+              {/* <h2 id="forecast-high">↑ {parseInt(this.props.forecast.daily.data[0].temperatureHigh)}°</h2>
+              <h2 id="forecast-low">↓ {parseInt(this.props.forecast.daily.data[0].temperatureLow)}°</h2> */}
               <h2 id="feels-temp">Feels like <span>{parseFloat(this.props.data.feelsLike).toFixed(1)}°</span></h2>
               {/* <h2 id="indoor-temp">Inside {this.props.data.tempinf}°</h2> */}
             </div>             
